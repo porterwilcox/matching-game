2de322cdcbd016b6b94873c5f810a7b51f644e08
Porter Wilcox <wilcox.porter@gmail.com>
2018-07-31 22:22:02 -0600

    more-scripty

--- assets/scripty.js
+++ assets/scripty.js
@@ -1,3 +1,22 @@
+//Variables
 let box = document.getElementsByClassName('box');
+// console.log(box);
 
-console.log(box[0]);
+
+
+//Functions
+let hideStart = function () {
+    document.querySelector(".start").style.display = 'none';
+}
+
+let randomBox = function() {
+    let i = Math.floor(Math.random()*8);
+    box[i].style.backgroundColor = "whitesmoke";
+    box[i].style.boxShadow = "0 0 50px white";
+}
+
+
+
+//Events
+box[5].addEventListener('click', hideStart);
+box[5].addEventListener('click', randomBox);
