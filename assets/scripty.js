19b504d7d8b266c3dc97a57d19d83c0846c9e438
Porter Wilcox <wilcox.porter@gmail.com>
2018-08-01 23:23:36 -0600

    works-but-got-to-play-slow-not-great

--- assets/scripty.js
+++ assets/scripty.js
@@ -8,6 +8,10 @@ let boxes = Array.from(box);
 let simonOff;
 let playerArr = [];
 let shiftUnwanted;
+let testArr = [5];
+let testIndex;
+let myIndex;
+let empower;
 
 
 //Functions
@@ -16,12 +20,12 @@ let hideStart = function () {
     shiftUnwanted = setTimeout(shifty, 1000);
 }
 
-let shifty = function() {
+let shifty = function () {
     playerArr.shift();
 }
 
-let randomBox = function() {
-    let i = Math.floor(Math.random()*8);
+let randomBox = function () {
+    let i = Math.floor(Math.random() * 8);
     box[i].style.backgroundColor = "whitesmoke";
     box[i].style.boxShadow = "0 0 50px white";
     compArr.push(i);
@@ -29,35 +33,45 @@ let randomBox = function() {
     simonOff = setTimeout(stopSimon, 4000);
 }
 
-let boxOff = function() {
+let boxOff = function () {
     let myBox = boxes.find(b => b.style.backgroundColor == "whitesmoke");
     myBox.style.backgroundColor = "transparent";
     myBox.style.boxShadow = "none";
 }
 
-let simon = function() {
+let simon = function () {
     nSimonInt = setInterval(randomBox, 1000);
 }
 
-let stopSimon = function() {
+let stopSimon = function () {
     clearInterval(nSimonInt);
+    testArr = compArr.slice();
+    givePower();
+}
+
+let givePower = function () {
     for (let i = 0; i < boxes.length; i++) {
         boxes[i].style.pointerEvents = "all";
     }
 }
 
-let colorChange = function() {
+let colorChange = function () {
     this.style.backgroundColor = "whitesmoke";
     this.style.boxShadow = "0 0 50px white";
     nBoxOff = setTimeout(boxOff, 250);
 }
 
 let playerMatch = function () {
-   let myIndex = playerArr.push(boxes.findIndex(b => b.style.backgroundColor == "whitesmoke"));
-    let testArr = compArr.slice();
-    
-    console.log(testArr)
-    console.log(playerArr)
+    playerArr.push(boxes.findIndex(b => b.style.backgroundColor == "whitesmoke"));
+    myIndex = playerArr.shift();
+    testIndex = testArr.shift();
+    console.log(testArr);
+    if (testArr.length == undefined) {
+        simon();
+    }
+    else if (myIndex !== testIndex) {
+        alert("You Lose.")
+    }
 }
 
 
